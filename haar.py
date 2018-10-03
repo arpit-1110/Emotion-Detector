cd876ed3e42188204bfb86b36b30774b3060a7d7
safwankdb <safwankdb@iitb.ac.in>
2018-10-04 01:38:33 +0530

    Added API

--- haar.py
+++ haar.py
@@ -1,22 +1,31 @@
-import numpy as np 
+import numpy as np
 import cv2
+import os
 
-face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(
+    'haarcascades/haarcascade_frontalface_default.xml')
 # eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 
-img = cv2.imread('banda.jpg')
+print('Enter image name')
+img = input()
+if img not in os.listdir():
+    print('No such file, ploxx be serious')
+    exit()
+img = cv2.imread(img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-i = 0 
+i = 0
 im = []
-for (x,y,w,h) in faces:
-	im.append(img[y:y+h, x:x+w])
-	roi_gray = gray[y:y+h, x:x+w]
-	roi_color = img[y:y+h, x:x+w]
+for (x, y, w, h) in faces:
+    im.append(img[y:y + h, x:x + w])
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = img[y:y + h, x:x + w]
 
-for i in im :
-	cv2.imshow('img',i)
-	cv2.waitKey(10000)
-	# cv2.destroyAllWindows()
-cv2.destroyAllWindows()	
+for i in im:
+    cv2.imshow('img', img)
+    cv2.waitKey(4000)
+    cv2.imshow('img', i)
+    cv2.waitKey(4000)
+    # cv2.destroyAllWindows()
+cv2.destroyAllWindows()
